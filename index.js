517d7c3723902ffeeef0229d371d3100ae0ff952
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-25 20:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const lowerCaseDrivers = drivers.map(function(name) {
+    return Object.assign({}, name.toLowerCase());
+  });
+}
