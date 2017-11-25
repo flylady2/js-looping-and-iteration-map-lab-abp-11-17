3233847fe76621b35eba515e6421fb47ade74e4b
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-25 21:54:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+function nameToAttributes(array) {
+  return array.map(function(name) {
+    let firstName = name.split(" ")[0];
+    let lastName = name.split(" ")[1];
+    return {firstName: firstName, lastName: lastName};
+  })
+}
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver['name']} is from ${driver['hometown']}`;
+  });
+}
