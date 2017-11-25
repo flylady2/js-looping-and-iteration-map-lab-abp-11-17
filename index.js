0bc1f13af5f953e914466572acc167f81e3088db
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-25 21:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function lowerCaseDrivers(array) {
 }
 function nameToAttributes(array) {
   return array.map(function(name) {
-    let firstName = name.split("")[0];
-    let lastName = name.split("")[1];
+    let firstName = name.split(" ")[0];
+    let lastName = name.split(" ")[1];
     return {firstName: firstName, lastName: lastName};
   })
 }
